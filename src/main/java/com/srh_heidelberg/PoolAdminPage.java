de1dcbcc18f0ae063dc1908b23c11b3a1cfd8b02
Sanket <mcbc83748@gmail.com>
2018-05-23 19:33:06 +0530

    Committing code for PoolAdminPage

--- src/main/java/com/srh_heidelberg/PoolAdminPage.java
+++ src/main/java/com/srh_heidelberg/PoolAdminPage.java
@@ -15,7 +15,7 @@ public class PoolAdminPage {
         dmlOperations.getPoolMembers(PoolAdmin);
         PoolAdminMemberID = PoolAdmin;
         System.out.println("Selection the field to update :");
-        System.out.println("1. Add Payments \n2.Pick Winner");
+        System.out.println("1. Add Payments \n2.Pick Winner \n3.Update Pool Details");
         int option = scanner.nextInt();
         performSelection(option);
 
@@ -34,6 +34,79 @@ public class PoolAdminPage {
                     System.out.println("You are Not Admin for PoolID :"+PoolID +"\nPlease retry.");
                     PoolAdminPage.AdminOperationSelect(PoolID);
                 }
+                break;
+            case 2: break;
+            case 3:
+                updatePooldetails();
+                break;
+
+
+        }
+
+    }
+
+
+    private static void updatePooldetails() {
+        System.out.println("Selection the field to update :");
+        System.out.println("1. Pool Name  \n 2. Enter Duration \n 3. Enter Strength \n 4. Enter Current Counter");
+        System.out.println("5. Enter Individual share \n 6. Enter Monthly Takeaway \n 7. Enter Start date \n 8. Enter End date \n 9. Enter Meet up Date ");
+        System.out.println("10.Enter Deposit Date \n 11. Enter Late fee charge " ) ;
+        int option = scanner.nextInt();
+        updateOperation(option);
+    }
+
+
+    private static void updateOperation(int option) {
+        UpdatePoolDetails updatePoolDetails = new UpdatePoolDetails();
+
+        switch (option) {
+            case 1:
+                updatePoolDetails.updatePoolName();
+                updatePooldetails();
+                break;
+            case 2:
+                updatePoolDetails.updateDuration();
+                updatePooldetails();
+                break;
+            case 3:
+                updatePoolDetails.updateStrength();
+                updatePooldetails();
+                break;
+            case 4:
+                updatePoolDetails.updateCurrentCounter();
+                updatePooldetails();
+                break;
+            case 5:
+                updatePoolDetails.updateIndividualShare();
+                updatePooldetails();
+                break;
+            case 6:
+                updatePoolDetails.updateMonthlyTakeaway();
+                updatePooldetails();
+                break;
+            case 7:
+                updatePoolDetails.updateStartDate();
+                updatePooldetails();
+                break;
+            case 8:
+                updatePoolDetails.updateEndDate();
+                updatePooldetails();
+                break;
+            case 9:
+                updatePoolDetails.updateMeetupDate();
+                updatePooldetails();
+                break;
+            case 10:
+                updatePoolDetails.updateDepositDate();
+                updatePooldetails();
+                break;
+            case 11:
+                updatePoolDetails.updateLateFeeCharge();
+                updatePooldetails();
+                break;
+            case 12:
+                break;
+            default:
                 break;
         }
     }
